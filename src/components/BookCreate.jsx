5af037a2f51d5bb4cab54d1a1aa93ae69f06c235
AfazPaul1 <afazpaul@gmail.com>
2025-01-16 23:14:21 +0530

    used mui grid

--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid2';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 function BookCreate({onCreate}) {
@@ -11,6 +12,7 @@ function BookCreate({onCreate}) {
     const onSubmit = (event) => {
         event.preventDefault()
         onCreate(book)
+        setBook("")
 
     }
 
@@ -18,19 +20,30 @@ function BookCreate({onCreate}) {
     return (
         <form onSubmit={onSubmit}>
             <Box>
-                <Typography variant="h6" gutterBottom>
-                    Add a Book
-                </Typography>
-                <TextField 
-                onChange={(event) => {
-                    setBook(event.target.value)
-                }} 
-                value={book}
-                id="outlined-basic" 
-                label="Title" 
-                variant="outlined" />
-                <Button onClick={onSubmit} variant="contained">Submit</Button>
+                <Grid sx={{ backgroundColor: 'lightgreen', padding: 1 }} container spacing={2}>
+                    <Grid sx={{ backgroundColor: 'lightblue', padding: 1 }} item size={12}>
+                        <Typography variant="h6" gutterBottom>
+                            Add a Book
+                        </Typography>
+                    </Grid>
+                    <Grid  sx={{ background: 'lightyellow', padding: 1 }} container columns = {2}>
+                        <Grid item xs={8} >
+                            <TextField 
+                            onChange={(event) => {
+                                setBook(event.target.value)
+                            }} 
+                            value={book}
+                            id="outlined-basic" 
+                            label="Title" 
+                            variant="outlined" />
+                        </Grid>
+                        <Grid display="flex" alignItems="center" item xs={4}>
+                            <Button onClick={onSubmit} variant="contained">Submit</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Box>
+        
         </form>
     )
 }
